65d70b24c726bb1c1678db39119311adfdda1c1e
Akash Nigam <56196402+nigakash05@users.noreply.github.com>
2020-05-21 23:27:34 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername= "akashdb05.mariadb.database.azure.com:3306";
+$username= "rootadmin@akashdb05";
+$password= "Akash1234567";
+$dbname= "akashdb05";
+
+$conn= my_sqli_real_connect($servername, $username, $password, $dbname);
+
+?>
